df400b742169c3bdc7bac8d359f5af3d226cca82
chaosannals <chaosannals@outlook.com>
2021-11-15 20:12:24 +0800

    file

--- olddemo/src/main/java/trial/App.java
+++ olddemo/src/main/java/trial/App.java
@@ -1,5 +1,8 @@
 package trial;
 
+import java.io.*;
+import java.nio.file.*;
+
 import trial.util.*;
 
 public class App {
@@ -8,9 +11,29 @@ public class App {
     }
 
     public static void main(String[] args) {
-        Timing.timing(() -> {
-            
-            return null;
-        });
+        try {
+            // String c = Files.readString(Path.of("license"));
+            String c = readAll("license");
+            System.out.println(c);
+            Timing.timing(() -> {
+
+                return null;
+            });
+        } catch (Exception e) {
+
+        }
+    }
+
+    private static String readAll(String path) throws IOException {
+        try (BufferedReader r = new BufferedReader(new FileReader(path))) {
+            StringBuilder b = new StringBuilder();
+            char[] buffer = new char[1024];
+            int n = 0;
+            while ((n = r.read(buffer)) != -1) {
+                String d = String.valueOf(buffer, 0, n);
+                b.append(d);
+            }
+            return b.toString();
+        }
     }
 }
